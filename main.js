a12508870ca9aeea35291e59d7488a35b5da1b2d
David silva <89320886+DsK-David@users.noreply.github.com>
2021-11-15 15:25:42 -0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+function verificar(){
+        var vel=document.getElementById("vel")
+        var resu=document.getElementById("resultado")
+        var agora=new Date()
+        var hora=agora.getHours("")
+        if(vel.value > 60){
+                resu.innerHTML="Você acabou de ser multado por ecesso de velocidade ⛔"
+        }
+        if(vel.value <= 60,vel.value > 0){
+          resu.innerHTML=" andou no limite de velocidade use sempre o sinto de segurança"
+        }
+        if(vel.value==""){
+        				resu.innerHTML="verifica os dados"
+        }
+        
+        if(hora > 22){
+              resu.innerHTML="não multamos a noite so a polícia regional"  
+        }
+        
+}
